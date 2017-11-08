517c8e8cf47f61751c478b08aaebc84985a21dad
James Fernandez <jffernan@comcast.net>
2017-11-08 14:41:13 -0800

    Update Invitation.js

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -4,9 +4,9 @@ export default class Invitation extends React.Component {
   render() {
     return (
       <div>
-        <h1>You've been invited!</h1>
+        <h1>You have been invited!</h1>
         {this.props.children}
       </div>
     );
   }
-}
+};
